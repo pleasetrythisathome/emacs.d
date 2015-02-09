daff9f6233823b8db3eb82f8da464eb8fa6408d2
Dylan Butman <dbutman@gmail.com>
2015-02-09 18:34:14 -0500

    load files

--- init.el
+++ init.el
@@ -3,10 +3,16 @@
 ;; Use this file to initiate the pack configuration.
 ;; See README for more information.
 
-(add-to-list 'custom-theme-load-path "~/.live-packs/pleasetrythisathome-pack/lib/themes/")
-(live-load-config-file "packages.el")
-(live-load-config-file "general.el")
-(live-load-config-file "prodigy.el")
-(live-load-config-file "clojure.el")
-(live-load-config-file "web.el")
-(live-load-config-file "bindings.el")
+(require 'cask "/usr/local/Cellar/cask/0.7.2/cask.el")
+(cask-initialize)
+(require 'pallet)
+(pallet-mode t)
+
+(add-to-list 'custom-theme-load-path "~/.emacs.d/lib/themes/")
+(add-to-list 'load-path "~/.emacs.d/config")
+
+(load "general.el")
+(load "clojure.el")
+(load "prodigy.el")
+(load "web.el")
+(load "bindings.el")
